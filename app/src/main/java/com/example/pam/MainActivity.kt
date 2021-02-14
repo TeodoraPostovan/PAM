70bc3fac40960fa5542569f9913f890259fc1515
Teodora Postovan <postovan.teodora@gmail.com>
2021-02-14 17:22:21 +0200

    Adjust layouts

--- app/src/main/java/com/example/pam/MainActivity.kt
+++ app/src/main/java/com/example/pam/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.pam
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        }
     }
-}
