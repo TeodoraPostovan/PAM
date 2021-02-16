86463048ff058c87f142596f093f9c8639da82b3
Teodora Postovan <postovan.teodora@gmail.com>
2021-02-16 19:42:19 +0200

    add transfer info between screen

--- app/src/main/java/com/example/pam/Activity2.kt
+++ app/src/main/java/com/example/pam/Activity2.kt
@@ -1,15 +1,41 @@
 package com.example.pam
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Switch
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_2.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class Activity2 : AppCompatActivity() {
 
-
+private var data02 : String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_2)
 
+
+
+        Btn2.setOnClickListener{
+            if(TextUtils.isEmpty(data2.text.toString().trim())) {
+                Toast.makeText(this, "Enter something", Toast.LENGTH_SHORT).show()
+            } else {
+                Intent(this, MainActivity::class.java).also{
+                    it.putExtra("data from second screen", data2.text.toString().trim())
+                    startActivity(it)
+                }
+
+            }
+        }
+
+        intent?.let{
+            data02 = it.getStringExtra("data from first screen")
+        }
+        incoming_data_from_screen_1.text = data02
+
     }
+
 }
--- app/src/main/java/com/example/pam/MainActivity.kt
+++ app/src/main/java/com/example/pam/MainActivity.kt
@@ -5,12 +5,36 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_2.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var data01 : String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
+        Btn.setOnClickListener{
+            if(TextUtils.isEmpty(data1.text.toString().trim())) {
+                Toast.makeText(this, "Enter something", Toast.LENGTH_SHORT).show()
+            } else {
+                Intent(this, Activity2::class.java).also{
+                    it.putExtra("data from first screen", data1.text.toString().trim())
+                    startActivity(it)
+                }
+
+            }
         }
+
+        intent?.let{
+            data01 = it.getStringExtra("data from second screen")
+        }
+        incoming_data_from_screen_2.text = data01
+
+
     }
+}
